dcb0b4f2d42c209889bf13116705674bf81700fc
cijang74 <82044367+cijang74@users.noreply.github.com>
2021-09-21 22:10:54 +0900

    2-2

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,30 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int a = 3, b = 3, c = 0, d = 0;
-        c = ++a;
-        d = b++;
+        Scanner scan = new Scanner(System.in);
+        String weatherReport = null;
+        String newsMessage = null;
 
-        System.out.println("a = 3일 때 c = ++a 적용 후 결과 a = " + a + ",c = " + c);
-        System.out.println("b = 3일 때 d = b++ 적용 후 결과 d = " + b + ",c = " + d);
+        System.out.print("오늘의 기상 특보를 입력하세요: ");
+        weatherReport = scan.next();
 
-        c = --a;
-        d = b--;
+        System.out.println("일부 지방에 " + weatherReport + "가 발효중입니다.");
+        newsMessage = "출근길 " ;
 
-        System.out.println("a = 4일 때 c = --a 적용 후 결과 a = " + a + ",c = " + c);
-        System.out.println("b = 4일 때 d = b-- 적용 후 결과 d = " + b + ",c = " + d);
+        if (weatherReport.equals ("대설특보")){
+            newsMessage += "눈길";
+        }
+
+        else if (weatherReport.equals ("호우특보")){
+            newsMessage += "빗길";
+        }
+
+        else if (weatherReport.equals ("강풍주의보")){
+            newsMessage += "안전";
+        }
+
+        newsMessage += " 운전에 유의해야 합니다.";
+        System.out.println(newsMessage);
 
         return;
     }
