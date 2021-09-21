4edf1c1f34895dee36ecaa83f6196e9265ce9796
cijang74 <82044367+cijang74@users.noreply.github.com>
2021-09-22 00:03:23 +0900

    2-8

--- src/Main.java
+++ src/Main.java
@@ -4,20 +4,35 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int score = 0;
+        String player1 = null;
+        String player2 = null;
 
-        System.out.println("0점 ~ 100점 사이의 점수를 입력하세요: ");
-        score = scan.nextInt();
+        System.out.print("A양, 가위, 바위, 보 중 하나를 선택하세요: ");
+        player1 = scan.nextLine().trim();
+        System.out.println(player1 + "를 냈습니다.");
 
-        switch (score / 10){
-            case 10: case 9:
-                System.out.println("와! ");
-            case 8:
-                System.out.println("끝내주게 !");
-            case 7:
-                System.out.println("잘 ");
-            default:
-                System.out.println("했다");
+        System.out.print("B군, 가위, 바위, 보 중 하나를 선택하세요: ");
+        player2 = scan.nextLine().trim();
+        System.out.println(player2 + "를 냈습니다.");
+
+        if (player1.equals(player2))
+        {
+            System.out.println("\n비겼습니다.");
+        }
+
+        else if (player1.equals("가위") && player2.equals("보"))
+        {
+            System.out.println("A양이 이겼습니다.");
+        }
+
+        else if (player1.equals("바위") && player2.equals("가위"))
+        {
+            System.out.println("A양이 이겼습니다.");
+        }
+
+        else if (player1.equals("보") && player2.equals("바위"))
+        {
+            System.out.println("A양이 이겼습니다.");
         }
 
         return;
