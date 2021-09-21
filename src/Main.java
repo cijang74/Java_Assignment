c96bbdfdab49ed77df9223885d1516ee688bf31c
cijang74 <82044367+cijang74@users.noreply.github.com>
2021-09-22 00:37:49 +0900

    2-10

--- src/Main.java
+++ src/Main.java
@@ -4,49 +4,27 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int month = 0;
+        int year = 0;
 
-        System.out.print("1월 ~ 12월 사이의 월을 입력하세요.: ");
-        month = scan.nextInt();
+        System.out.print("년도를 입력하세요: ");
+        year = scan.nextInt();
 
-        switch (month){
-            case 1:
-                System.out.println("1월의 영어 이름은 January 입니다.");
-                break;
-            case 2:
-                System.out.println("2월의 영어 이름은 February 입니다.");
-                break;
-            case 3:
-                System.out.println("3월의 영어 이름은 March 입니다.");
-                break;
-            case 4:
-                System.out.println("4월의 영어 이름은 April 입니다.");
-                break;
-            case 5:
-                System.out.println("5월의 영어 이름은 May 입니다.");
-                break;
-            case 6:
-                System.out.println("6월의 영어 이름은 June 입니다.");
-                break;
-            case 7:
-                System.out.println("7월의 영어 이름은 July 입니다.");
-                break;
-            case 8:
-                System.out.println("8월의 영어 이름은 August 입니다.");
-                break;
-            case 9:
-                System.out.println("9월의 영어 이름은 September 입니다.");
-                break;
-            case 10:
-                System.out.println("10월의 영어 이름은 October 입니다.");
-                break;
-            case 11:
-                System.out.println("11월의 영어 이름은 November 입니다.");
-                break;
-            default:
-                System.out.println("12월의 영어 이름은 December 입니다.");
-                break;
+        if ((year % 400) == 0)
+        {
+            System.out.println(year + "년은 윤년입니다.");
         }
+
+        else if ((year % 100) == 0)
+        {
+            System.out.println(year + "년은 평년입니다.");
+        }
+
+        else if ((year % 4) == 0)
+        {
+            System.out.println(year + "년은 윤년입니다.");
+        }
+
+        else System.out.println(year + "년은 평년입니다.");
 
         return;
     }
