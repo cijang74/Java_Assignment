9b8a50befda5aa5ff9a9e491d9c2c4a2587b8d20
cijang74 <82044367+cijang74@users.noreply.github.com>
2021-10-06 00:33:53 +0900

    4-5

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,15 @@
 import java.util.Scanner;
 
-class AirConditioner
+class RCCar
 {
     public final boolean ON = true;
     public final boolean OFF = false;
 
     private boolean power;
-    private int temperature;
-    private int windStrength;
 
-    public AirConditioner()
+    public RCCar()
     {
         this.power = OFF;
-        this.temperature = 18;
-        this.windStrength = 10;
     }
 
     public String clickPower()
@@ -32,22 +28,22 @@ class AirConditioner
 
     public String clickUp()
     {
-        return "현재 온도는" + (++this.temperature) + "도입니다.";
+        return "직진합니다";
     }
 
     public String clickDown()
     {
-        return "현재 온도는" + (--this.temperature) + "도입니다.";
+        return "후진합니다";
     }
 
     public String clickLeft()
     {
-        return "현재 풍량은" + (--this.windStrength) + "입니다.";
+        return "좌회전합니다";
     }
 
     public String clickRight()
     {
-        return "현재 풍량은" + (++this.windStrength) + "입니다.";
+        return "우회전합니다";
     }
 }
 
@@ -58,32 +54,32 @@ public class Main
         Scanner scan = new Scanner(System.in);
         String result = "";
 
-        AirConditioner aircon = new AirConditioner();
+        RCCar rcCar = new RCCar();
 
         do
         {
-            System.out.print("에어컨 리모콘의 상하좌우, 전원, 종료 중 하나를 입력하세요: ");
+            System.out.print("RC카 리모콘의 상하좌우, 전원, 종료 중 하나를 입력하세요: ");
             result = scan.next();
 
             if(result.equals("전원"))
             {
-                System.out.println(aircon.clickPower());
+                System.out.println(rcCar.clickPower());
             }
             else if (result.equals("상"))
             {
-                System.out.println(aircon.clickUp());
+                System.out.println(rcCar.clickUp());
             }
             else if (result.equals("하"))
             {
-                System.out.println(aircon.clickDown());
+                System.out.println(rcCar.clickDown());
             }
             else if (result.equals("좌"))
             {
-                System.out.println(aircon.clickLeft());
+                System.out.println(rcCar.clickLeft());
             }
             else if (result.equals("우"))
             {
-                System.out.println(aircon.clickRight());
+                System.out.println(rcCar.clickRight());
             }
         } while(!result.equals("종료"));
         return;
