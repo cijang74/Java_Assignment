fffacc3207848d595e363ac392894ab7138061f7
cijang74 <82044367+cijang74@users.noreply.github.com>
2021-10-06 00:24:37 +0900

    4-4

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,19 @@
 import java.util.Scanner;
 
-class TV
+class AirConditioner
 {
     public final boolean ON = true;
     public final boolean OFF = false;
 
     private boolean power;
-    private int channel;
-    private int volume;
+    private int temperature;
+    private int windStrength;
 
-    public TV()
+    public AirConditioner()
     {
         this.power = OFF;
-        this.channel = 100;
-        this.volume = 10;
+        this.temperature = 18;
+        this.windStrength = 10;
     }
 
     public String clickPower()
@@ -32,22 +32,22 @@ class TV
 
     public String clickUp()
     {
-        return "현재 채널은" + (++this.channel) + "번입니다.";
+        return "현재 온도는" + (++this.temperature) + "도입니다.";
     }
 
     public String clickDown()
     {
-        return "현재 채널은" + (--this.channel) + "번입니다.";
+        return "현재 온도는" + (--this.temperature) + "도입니다.";
     }
 
     public String clickLeft()
     {
-        return "현재 음량은" + (--this.volume) + "입니다.";
+        return "현재 풍량은" + (--this.windStrength) + "입니다.";
     }
 
     public String clickRight()
     {
-        return "현재 음량은" + (++this.volume) + "입니다.";
+        return "현재 풍량은" + (++this.windStrength) + "입니다.";
     }
 }
 
@@ -58,32 +58,32 @@ public class Main
         Scanner scan = new Scanner(System.in);
         String result = "";
 
-        TV tv = new TV();
+        AirConditioner aircon = new AirConditioner();
 
         do
         {
-            System.out.print("TV 리모콘의 상하좌우, 전원, 종료 중 하나를 입력하세요: ");
+            System.out.print("에어컨 리모콘의 상하좌우, 전원, 종료 중 하나를 입력하세요: ");
             result = scan.next();
 
             if(result.equals("전원"))
             {
-                System.out.println(tv.clickPower());
+                System.out.println(aircon.clickPower());
             }
             else if (result.equals("상"))
             {
-                System.out.println(tv.clickUp());
+                System.out.println(aircon.clickUp());
             }
             else if (result.equals("하"))
             {
-                System.out.println(tv.clickDown());
+                System.out.println(aircon.clickDown());
             }
             else if (result.equals("좌"))
             {
-                System.out.println(tv.clickLeft());
+                System.out.println(aircon.clickLeft());
             }
             else if (result.equals("우"))
             {
-                System.out.println(tv.clickRight());
+                System.out.println(aircon.clickRight());
             }
         } while(!result.equals("종료"));
         return;
