28b0e7eaccc2310ad51eaf439fb9c27ad587cff1
cijang74 <82044367+cijang74@users.noreply.github.com>
2021-11-07 03:23:36 +0900

    6-9

--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,10 @@ public class Main
 {
     public static void main (String[] args)
     {
-        JFrame frame = new JFrame("TV");
+        JFrame frame = new JFrame("에어콘");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        TVPanel panel = new TVPanel(); // TVPanel 객체 생성, panel에 저장
+        AirConditionerPanel panel = new AirConditionerPanel(); // TVPanel 객체 생성, panel에 저장
         frame.getContentPane().add(panel); // panel을 프레임에 추가
 
         frame.pack();
